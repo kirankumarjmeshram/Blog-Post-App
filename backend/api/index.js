6e9bdba31e667cdf61b2111b0818402aa2644e20
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2022-05-29 17:24:20 +0530

    working

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
-app.use("/user",(req,res)=>{
-    console.log("Hello World 234")
-})
+dotenv.config();
+
+// mongoose
+//   .connect(process.env.MONGO_URL)
+//   .then(console.log("Connected to MongoDB"))
+//   .catch((err) => console.log(err));
+
+mongoose.connect('mongodb://127.0.0.1:27017/blog_post')
 
 app.listen(3000,()=>{
     console.log("Listening on port 3000")
